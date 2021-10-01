8e1a26aa22ac0f072a9e98158312a40cbbb7bdd1
Yannick Severo <severoyannick@gmail.com>
2021-10-01 22:30:14 +0200

    Rechercher et installer un module vous permettant de facilement afficher les heures des différents fuseaux horaires

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// TODO
+// Module Dayjs et les plugins pour gérer les timezones et les formats "localisés"
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+const localizedFormat = require('dayjs/plugin/localizedFormat');
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(localizedFormat);
+require('dayjs/locale/fr'); 
+dayjs.locale('fr');
