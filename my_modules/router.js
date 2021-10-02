0b58514bf0136a6b60bbd64d0eac689eb22adc44
Yannick Severo <severoyannick@gmail.com>
2021-10-02 10:03:13 +0200

    Créer une route dynamique (= avec un paramètre) pour le détail des capitales

--- my_modules/router.js
+++ my_modules/router.js
@@ -1,13 +1,32 @@
 // Récupération des dépendences
-const express = require('express');
+const express = require("express");
 // Création du routeur express
 const router = express.Router();
+// La liste des capitales
+const listOfCapitals = require("./capitalCities");
 
 // Page d'accueil
-router.get('/', (req, res) => {
-  res.sendFile('index.html', {
-    root: __dirname + '/views/',
-  })
-;})
+router.get("/", (req, res) => {
+  res.sendFile("index.html", {
+    root: __dirname + "/views/",
+  });
+});
+
+// Route paramétrée qui revoie /city/leNomDeLaCapitale
+router.get("/city/:capital", (req, res) => {
+  const askedCapital = req.params.capital;
+  let foundCapital;
+
+  for (testCapital of listOfCapitals) {
+    if (testCapital.name.toLowerCase() === askedCapital) {
+      foundCapital = testCapital;
+      res.send(`
+        <h1>${foundCapital.name}</h1>
+        <p>${foundCapital.tz}</p>
+        <a href='/'>Retour à l'accueil<a>
+      `);
+    }
+  }
+});
 
 module.exports = router;
